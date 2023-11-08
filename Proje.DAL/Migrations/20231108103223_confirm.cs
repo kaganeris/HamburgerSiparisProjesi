23f15692872fefd5e8a65548ded16a0281b46b75
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2023-11-08 15:58:32 +0300

    E-mail onay kodu

---
+++ Proje.DAL/Migrations/20231108103223_confirm.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proje.DAL.Migrations
+{
+    public partial class confirm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConfirmCode",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmCode",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Proje.UI/Controllers/ConfirmMailController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Proje.BLL.Models.VMs;
+using Proje.DATA.Entities;
+
+namespace Proje.UI.Controllers
+{
+    public class ConfirmMailController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public ConfirmMailController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var value = TempData["Mail"];
+            ViewBag.Email = value;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ConfirmMailVM confirmMailVM)
+        {
+            if(confirmMailVM.Email != null)
+            {
+                var user = await userManager.FindByEmailAsync(confirmMailVM.Email);
+                if (user.ConfirmCode == confirmMailVM.ConfirmCode)
+                {
+                    user.EmailConfirmed = true;
+                    await userManager.UpdateAsync(user);
+                    return RedirectToAction("Login", "User");
+                }
+                ViewBag.Uyarı = "Hatalı Onay Kodu!";
+            }
+            return View();
+        }
+    }
+}
