79a3c31c0ff3e2fb06e34381d96d5c0dac591b2b
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2023-11-10 14:43:02 +0300

    şifre yenileme

---
+++ Proje.BLL/Validations/UpdateUserDTOValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Proje.BLL.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje.BLL.Validations
+{
+    public class UpdateUserDTOValidator : AbstractValidator<UpdateUserDTO>
+    {
+        public UpdateUserDTOValidator()
+        {
+            RuleFor(x => x.Ad).NotEmpty().WithMessage("İsim boş geçilemez!");
+            RuleFor(x => x.Soyad).NotEmpty().WithMessage("Soyad boş geçilemez!");
+            RuleFor(x => x.Cinsiyet).NotNull().WithMessage("Cinsiyet boş geçilemez!");
+            RuleFor(x => x.DogumTarihi).NotNull().WithMessage("Doğum tarihi boş geçilemez!");
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Telefon Numarası boş geçilemez!");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email boş geçilemez!");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Geçerli bir e-mail adresi giriniz");
+        }
+    }
+}
